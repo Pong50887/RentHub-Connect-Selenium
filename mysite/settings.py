0e07743051ab01b84c4e1caab868314b85da5829
NewOsakA <115053961+NewOsakA@users.noreply.github.com>
2024-10-07 22:09:17 +0700

    add Room models

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "renthub.apps.RenthubConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'renthub'
 ]
 
 MIDDLEWARE = [
--- renthub/models.py
+++ renthub/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    detail = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(max_length=200)
+    availability = models.BooleanField()
+
+    def __str__(self):
+        return self.detail
+
