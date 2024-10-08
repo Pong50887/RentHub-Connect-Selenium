ee480fe2effaa8727d56f1b5c649a6960bb92824
Karczel <92095164+Karczel@users.noreply.github.com>
2024-10-08 13:18:45 +0700

    create simple view for home, rental and payment page

--- renthub/urls.py
+++ renthub/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'renthub'
+
 urlpatterns = [
-    path("", views.home, name="index"),
+    path("", views.home, name="home"),
+    path("<int:room_id>/", views.room, name="rental"),
+    path("<int:room_id>/payment", views.room_payment, name="payment"),
+    path("<int:room_id>/payment/submit", views.submit_payment, name="submit"),
+
 ]
--- renthub/views.py
+++ renthub/views.py
@@ -1,5 +1,41 @@
-from django.http import HttpResponse
-
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from .models import Room, Rental, Renter
+from django.contrib import messages
 
 def home(request):
-    return HttpResponse("Hello, world. You are on the right track")
+    rooms = Room.objects.filter(availability=True)
+    return render(request, "renthub/home.html", {"rooms": rooms})
+
+
+def room(request, room_id):
+    room = Room.objects.get(id=room_id)
+    if room.availability == False:
+        return HttpResponseRedirect(reverse("renthub:home"))
+    return render(request, "renthub/rental.html", {"room": room})
+
+
+def room_payment(request, room_id):
+    room = Room.objects.get(id=room_id)
+    user = request.user
+    if room.availability == False:
+        messages.error(request, f"The room {room} is currently unavailable.")
+        return HttpResponseRedirect(reverse("renthub:home"))
+
+    return render(request, "renthub/payment.html", {"room": room})
+
+def submit_payment(request, room_id):
+    room = Room.objects.get(id=room_id)
+    user = request.user
+    # try:
+    #     renter = Renter.objects.get(id=user.id)
+    #     renter_id = renter.id
+    # except Renter.DoesNotExist:
+    #     messages.error(request, "You must be a registered renter to rent a room.")
+    #     return HttpResponseRedirect(reverse("renthub:payment"))
+    #
+    # if Rental.objects.filter(room=room_id, renter=renter_id).exists():
+    #     messages.info(request, "You have already rented this room.")
+
+    return render(request, "renthub/payment.html", {"room": room})
