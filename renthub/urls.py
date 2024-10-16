9c83a85030823ea84bc0378101496f2349561070
Karczel <92095164+Karczel@users.noreply.github.com>
2024-10-16 21:47:39 +0700

    Announcement tests.

--- renthub/urls.py
+++ renthub/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path("rental/<int:room_number>/payment/", views.RoomPaymentView.as_view(), name="payment"),
     path("rental/<int:room_number>/payment/submit/", views.submit_payment, name="submit"),
     path("rental/<int:room_number>/payment/cancel/", views.cancel_rental, name="cancel"),
-    path('announcement/<int:pk>', views.AnnouncementView.as_view(), name='announcement'),
+    path('announcement/<int:pk>', views.AnnouncementView.as_view(), name="announcement"),
 ]
