f23f7bce87478dc9c72fdfb8a343992b99260ae7
Phimnada CHIRACHOTSUPHAPHAT <phimnada.c@ku.th>
2024-10-28 14:03:19 +0700

    Make better UI for Rooms list page and Rent page

--- renthub/models/room_type.py
+++ renthub/models/room_type.py
@@ -6,8 +6,8 @@ class RoomType(models.Model):
     Represents a type of room with a shared image for each type.
     """
     type_name = models.CharField(max_length=100)
-    description = models.CharField(max_length=100, blank=True)
-    ideal_for = models.CharField(max_length=100, blank=True)
+    description = models.CharField(max_length=200, blank=True)
+    ideal_for = models.CharField(max_length=200, blank=True)
     image = models.ImageField(upload_to='room_images/', blank=True, null=True)
 
     def __str__(self):
