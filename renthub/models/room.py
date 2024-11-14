95cd68df233e5c417b8da95044cb9f3578358723
Karczel <92095164+Karczel@users.noreply.github.com>
2024-11-14 10:09:38 +0700

    Added room floor

--- renthub/models/room.py
+++ renthub/models/room.py
@@ -9,6 +9,7 @@ class Room(models.Model):
     Represents a room available for rent.
     """
     room_number = models.IntegerField(default=0)
+    room_floor = models.IntegerField(default=0)
     detail = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     room_type = models.ForeignKey(RoomType, on_delete=models.CASCADE, null=True, blank=True)
