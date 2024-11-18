616a51b58a2f6e36f95248fddb90a60b63618777
Karczel <92095164+Karczel@users.noreply.github.com>
2024-11-18 09:57:37 +0700

    fix PEP 8

--- renthub/utils.py
+++ renthub/utils.py
@@ -201,3 +201,4 @@ def admin_login(browser):
     browser.find_element(By.NAME, 'username').send_keys('rhadmin')
     browser.find_element(By.NAME, 'password').send_keys('renthub1234')
     browser.find_element(By.XPATH, '//input[@type="submit"]').click()
+    
