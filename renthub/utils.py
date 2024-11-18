2020f597a0e407949075b00042ba4ddb467f8d69
Karczel <92095164+Karczel@users.noreply.github.com>
2024-11-18 09:56:04 +0700

    --headless mode for selenium tests

--- renthub/utils.py
+++ renthub/utils.py
@@ -132,7 +132,7 @@ class Browser:
     def get_browser(cls):
         """Class method to initialize a headless Chrome WebDriver."""
         options = webdriver.ChromeOptions()
-        # options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument("--disable-extensions")
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
