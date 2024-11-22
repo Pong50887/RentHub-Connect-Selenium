fb7cffcdae563415218bab15c89655bfd1dfa54e
NewOsakA <115053961+NewOsakA@users.noreply.github.com>
2024-11-22 20:53:40 +0700

    Redirect normal user to home in UserProfileView

--- renthub/views/user_profile_view.py
+++ renthub/views/user_profile_view.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 from ..models import Renter
 
 
-class UserProfileView(View):
+class UserProfileView(LoginRequiredMixin, View):
+    """
+    View to display a user's profile.
+    Only accessible to superusers.
+    """
 
     def get(self, request, username):
-        try:
-            renter = Renter.objects.filter(username=username).first()
-        except Renter.DoesNotExist:
+        # Check if the user is a superuser
+        if not request.user.is_superuser:
             return redirect('renthub:home')
+
+        # Retrieve the renter's profile
+        renter = get_object_or_404(Renter, username=username)
         return render(request, 'renthub/user_profile.html', {'renter': renter})
