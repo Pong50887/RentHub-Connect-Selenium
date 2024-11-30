fd7cc758b8f3b9514dd933c8379de427ff95b897
Karczel <92095164+Karczel@users.noreply.github.com>
2024-11-30 11:26:49 +0700

    fix phone number field only accept numbers

--- renthub/models/renter.py
+++ renthub/models/renter.py
@@ -1,12 +1,19 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.core.validators import RegexValidator
 
 
 class Renter(User):
     """
     Represents a renter, extending Django's default User model.
     """
-    phone_number = models.CharField(max_length=10)
+    phone_number = models.CharField(max_length=10,
+                                    validators=[
+                                        RegexValidator(
+                                            regex=r'^\d{10}$',
+                                            message="Phone number must be exactly 10 digits."
+                                        )
+                                    ])
     thai_citizenship_id = models.CharField(max_length=13, blank=True, null=True, unique=True)
 
     class Meta:
