6d11e12b5474bcdf310448f3ee3752987a6ddf96
Karczel <92095164+Karczel@users.noreply.github.com>
2024-12-01 19:23:55 +0700

    Update renter_admin.py

--- renthub/admin/renter_admin.py
+++ renthub/admin/renter_admin.py
@@ -5,4 +5,5 @@ class RenterAdmin(admin.ModelAdmin):
     """
     Admin configuration for the Renter model.
     """
-    list_display = ('username', 'email', 'first_name', 'last_name', 'phone_number','thai_citizenship_id','thai_citizenship_id_image')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'phone_number','thai_citizenship_id',
+                    'thai_citizenship_id_image')
