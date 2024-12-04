61eee95e1a7bb2e8666b84c4dc353fe0ac1367b4
Karczel <92095164+Karczel@users.noreply.github.com>
2024-12-04 14:08:18 +0700

    catch no image error

--- renthub/admin/rental_admin.py
+++ renthub/admin/rental_admin.py
@@ -28,9 +28,9 @@ class RentalAdmin(admin.ModelAdmin):
         if change:
             original_status = Rental.objects.get(pk=obj.pk).status
 
-        # if obj.status == "approve" and not obj.renter.is_valid:
-        #     self.message_user(request, "The renter is not valid. Approval cannot be granted.", level="error")
-        #     return
+        if obj.status == "approve" and not obj.renter.is_valid:
+            self.message_user(request, "The renter is not valid. Approval cannot be granted.", level="error")
+            return
 
         super().save_model(request, obj, form, change)
 
